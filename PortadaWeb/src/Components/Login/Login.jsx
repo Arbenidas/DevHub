3c85c1c47ab5a528b0740e3bcf20126521a8c078
lllHunterlllSV <ia.edwin_hernandez@ufg.edu.sv>
2024-02-03 11:29:19 -0600

    login resuelto

--- PortadaWeb/src/Components/Login/Login.jsx
+++ PortadaWeb/src/Components/Login/Login.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
-
+import { useState } from 'react';
+import axios  from 'axios';
 
 const Login = () => {
+    const [username, setUsername]= useState('');
+    const [password, setPassword]= useState('');
+
+    const handleSubmit=() =>{
+        if (username.length === 0 || password.length === 0) {
+            alert('Ingrese un nombre de usuario y contraseña');
+        } else {
+            const url = "http://localhost/enquiry.php";
+            let fData = new FormData();
+            fData.append('username', username);
+            fData.append('password', password);
+            axios.post(url, fData)
+                .then(response => {
+                    if (response.data.success) {
+                        // Si el inicio de sesión es exitoso, redirige a la página deseada
+                        window.location.href = '../../';
+                      
+                    } else {
+                        alert(response.data.message);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    alert('Error de conexión.');
+                });
+        }
+    }
+
   return (
     <div className="login template d-flex justify-content-center align-items-center vh-100">
         <div className='form-container p-5 rounded bg-white'>
@@ -11,11 +40,11 @@ const Login = () => {
             <h3 class="text-center">Iniciar Sesión</h3>
             <div class="mb-2">
                 <label htmlFor="email">Email</label>
-                <input type="email" placeholder='Email' className='form-control'/>
+                <input type="email" placeholder='Email' value={username} onChange={(e)=>setUsername(e.target.value)} className='form-control' required/>
             </div>
             <div class="mb-2">
                 <label htmlFor="password">Contraseña</label>
-                <input type="password" placeholder='Contraseña' className='form-control'/>
+                <input type="password" placeholder='Contraseña' value={password} onChange={(e)=>setPassword(e.target.value)} className='form-control' required/>
             </div>
             <div>
                 <input type="checkbox" className='custom-control custom.checkbox' id="check" />
@@ -24,9 +53,8 @@ const Login = () => {
                 </label>
             </div>
             <div className='d-grid'>
-                <Link to="/" >
-                <button className='btn w-100'>Iniciar Sesión</button>
-                </Link>
+                
+                <input type="button" className='btn w-100' id="submit" onClick={handleSubmit} value={"Iniciar Sesión"} />
                 
 
             </div>
