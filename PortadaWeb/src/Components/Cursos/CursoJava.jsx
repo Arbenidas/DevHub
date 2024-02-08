357dc0f29f47d36592dd35365d30338fb1e02d58
Arbenidas <diego00clemente@gmail.com>
2024-02-08 09:51:45 -0600

    Conexion con base de datos para cursos (deficiente)

--- PortadaWeb/src/Components/Cursos/CursoJava.jsx
+++ PortadaWeb/src/Components/Cursos/CursoJava.jsx
@@ -1,25 +1,33 @@
 import ComponenteCompuesto from "../visualizador/ComponenteCompuesto";
 import NavbarCV from "../visualizador/Navbar";
 import axios from "axios";
+
+function regresarCurso(response) {
+  alert('hola')
+ 
+}
 export function CursoDefault() {
   axios
     .post("http://localhost/ccCursos.php")
     .then((response) => {
       if (response.data.success) {
+        
         // Si el inicio de sesión es exitoso, redirige a la página deseada
         console.log(response.data);
       } else {
-        alert(response.data.message);
+        return(regresarCurso(response.data))
+        alert(response.data);
       }
     })
     .catch((error) => {
       console.error("Error:", error);
       alert("No actualizo el curso.");
     });
-  return (
+     return (
     <>
       <NavbarCV />
-      <ComponenteCompuesto casa={{}} />
+      <ComponenteCompuesto/>
     </>
   );
+ 
 }
